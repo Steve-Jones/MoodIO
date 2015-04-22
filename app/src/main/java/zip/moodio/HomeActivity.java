f10eb94eac081268261c4bb5eb3f873373d6a7bb
Steve-Jones <steve@stevenjon.es>
2015-04-21 23:23:01 -0400

    Tabbed interface added

--- app/src/main/java/zip/moodio/HomeActivity.java
+++ app/src/main/java/zip/moodio/HomeActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -12,6 +13,30 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
+
+        //TABHOST
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+        //LOG TAB
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("log");
+        tabSpec.setContent(R.id.logTab);
+        tabSpec.setIndicator("Log");
+        tabHost.addTab(tabSpec);
+        //DATA TAB
+        tabSpec = tabHost.newTabSpec("data");
+        tabSpec.setContent(R.id.dataTab);
+        tabSpec.setIndicator("Data");
+        tabHost.addTab(tabSpec);
+        //HELP TAB
+        tabSpec = tabHost.newTabSpec("help");
+        tabSpec.setContent(R.id.helpTab);
+        tabSpec.setIndicator("Help");
+        tabHost.addTab(tabSpec);
+        //SETTINGS TAB
+        tabSpec = tabHost.newTabSpec("settings");
+        tabSpec.setContent(R.id.settingsTab);
+        tabSpec.setIndicator("Settings");
+        tabHost.addTab(tabSpec);
     }
 
 
