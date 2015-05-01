1fe9e20f993876cea52af7bae0a7b7bac3bd69be
zberman <zberman@outlook.com>
2015-05-01 14:21:01 -0400

    Added annotations to Log Screen

--- app/src/main/java/zip/moodio/HomeActivity.java
+++ app/src/main/java/zip/moodio/HomeActivity.java
@@ -6,12 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ExpandableListView;
 import android.widget.TabHost;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class HomeActivity extends ActionBarActivity {
